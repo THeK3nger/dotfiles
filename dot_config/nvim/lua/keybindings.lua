68b1474b9644d200fc921c9f25b123ade52c45e8
Davide Aversa <thek3nger@gmail.com>
2024-05-08 09:43:20 +0200

    Add .config/nvim/lua/keybindings.lua Add .config/nvim/lua/options.lua Add .config/nvim/lua/plugins.lua

---
+++ dot_config/nvim/lua/keybindings.lua
@@ -0,0 +1,25 @@
+
+local function map(mode, combo, mapping, opts)
+  local options = {noremap = true}
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, combo, mapping, options)
+end
+
+vim.g.mapleader = ','
+
+map('i', 'jk', '<Esc>')
+map('', '<C-E>', ':NvimTreeToggle<CR>')
+map('', '<C-t>', 'tabnew<CR>')
+
+map('n', '<silent> <A-Up>', ':wincmd k<CR>')
+map('n', '<silent> <A-Down>', ':wincmd j<CR>')
+map('n', '<silent> <A-Left>', ':wincmd h<CR>')
+map('n', '<silent> <A-Right>', ':wincmd l<CR>')
+
+-- Find files using Telescope command-line sugar.
+map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', {noremap=true})
+map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', {noremap=true})
+map('n', '<leader>fb', '<cmd>Telescope buffers<CR>', {noremap=true})
+map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', {noremap=true})
---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,28 @@
+
+local indent_size = 4
+
+vim.opt.tabstop = indent_size
+vim.opt.softtabstop = indent_size
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = true
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.title = true
+
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+
+vim.opt.undofile = true
+vim.opt.encoding = "utf-8"
+
+vim.opt.ignorecase = true
+vim.opt.conceallevel = 1
+vim.opt.mouse = "a"
+vim.opt.history = 1000
+
+vim.opt.autoread = true
