cb741692cd294413df709c43852f91cf57d63ca1
Davide Aversa <thek3nger@gmail.com>
2024-06-08 10:29:53 +0200

    Update .doom.d/custom.el

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -51,3 +51,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq fancy-splash-image (concat doom-private-dir "glyph_color.png"))
