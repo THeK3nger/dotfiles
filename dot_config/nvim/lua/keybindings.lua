a6ac7c595f9e5ed86a85551c044120e5a1b79c7c
Davide Aversa <thek3nger@gmail.com>
2025-02-18 16:11:41 +0100

    Update .config/nvim/lua/keybindings.lua

--- dot_config/nvim/lua/keybindings.lua
+++ dot_config/nvim/lua/keybindings.lua
@@ -1,16 +1,15 @@
-
 local function map(mode, combo, mapping, opts)
-  local options = {noremap = true}
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, combo, mapping, options)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, combo, mapping, options)
 end
 
 vim.g.mapleader = ','
 
 map('i', 'jk', '<Esc>')
-map('', '<C-E>', ':NvimTreeToggle<CR>')
+map("n", "<leader>d", ":Neotree toggle=true<CR>", { desc = "Neotree" })
 map('', '<C-t>', 'tabnew<CR>')
 
 map('n', '<silent> <A-Up>', ':wincmd k<CR>')
@@ -19,7 +18,7 @@ map('n', '<silent> <A-Left>', ':wincmd h<CR>')
 map('n', '<silent> <A-Right>', ':wincmd l<CR>')
 
 -- Find files using Telescope command-line sugar.
-map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', {noremap=true})
-map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', {noremap=true})
-map('n', '<leader>fb', '<cmd>Telescope buffers<CR>', {noremap=true})
-map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', {noremap=true})
+map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
+map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
+map('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
+map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
