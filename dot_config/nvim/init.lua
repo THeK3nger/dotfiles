735e954104ad0a1b48def1a52c9e4d333330e59f
Davide Aversa <thek3nger@gmail.com>
2025-03-30 17:48:58 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,11 +2,23 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('keybindings')
-require('options')
+require("keybindings")
+require("options")
 
-require('config.lazy')
+require("config.lazy")
 
 vim.o.guifont = "JetBrainsMono Nerd Font"
-vim.cmd.colorscheme "melange"
+vim.cmd.colorscheme("melange")
 vim.opt.termguicolors = true
+
+-- Diagnostic
+vim.diagnostic.config({
+	-- Use the default configuration
+	virtual_lines = true,
+
+	-- Alternatively, customize specific options
+	-- virtual_lines = {
+	--  -- Only show virtual line diagnostics for the current cursor line
+	--  current_line = true,
+	-- },
+})
