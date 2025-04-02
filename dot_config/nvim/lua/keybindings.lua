facfb7b26209a96112c86a89a1f86322b83930b7
Davide Aversa <thek3nger@gmail.com>
2025-04-02 17:49:45 +0200

    Update .config/nvim/lua/keybindings.lua

--- dot_config/nvim/lua/keybindings.lua
+++ dot_config/nvim/lua/keybindings.lua
@@ -1,38 +1,39 @@
 local function map(mode, combo, mapping, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend('force', options, opts)
-    end
-    -- Use vim.keymap.set for Lua functions
-    if type(mapping) == "function" then
-        vim.keymap.set(mode, combo, mapping, options)
-    else
-        vim.api.nvim_set_keymap(mode, combo, mapping, options)
-    end
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	-- Use vim.keymap.set for Lua functions
+	if type(mapping) == "function" then
+		vim.keymap.set(mode, combo, mapping, options)
+	else
+		vim.api.nvim_set_keymap(mode, combo, mapping, options)
+	end
 end
 
-vim.g.mapleader = ','
+vim.g.mapleader = ","
 
-map('i', 'jk', '<Esc>')
+map("i", "jk", "<Esc>")
 map("n", "<leader>d", ":Neotree toggle=true<CR>", { desc = "Neotree" })
-map('', '<C-t>', 'tabnew<CR>')
+map("", "<C-t>", "tabnew<CR>")
 
-map('n', '<silent> <A-Up>', ':wincmd k<CR>')
-map('n', '<silent> <A-Down>', ':wincmd j<CR>')
-map('n', '<silent> <A-Left>', ':wincmd h<CR>')
-map('n', '<silent> <A-Right>', ':wincmd l<CR>')
+map("n", "<silent> <A-Up>", ":wincmd k<CR>")
+map("n", "<silent> <A-Down>", ":wincmd j<CR>")
+map("n", "<silent> <A-Left>", ":wincmd h<CR>")
+map("n", "<silent> <A-Right>", ":wincmd l<CR>")
 
 -- Find files using Telescope command-line sugar.
-map('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
-map('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
-map('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
-map('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { noremap = true })
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { noremap = true })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { noremap = true })
+map("n", "<leader>fd", "<cmd>Telescope diagnostics<CR>", { noremap = true })
 
 -- Exit terminal mode
-map('t', '<leader><Esc>', '<C-\\><C-n>', { noremap = true })
+map("t", "<leader><Esc>", "<C-\\><C-n>", { noremap = true })
 
 -- Oil
-map('n', '-', '<CMD>Oil<CR>', { desc = "Open parent directory" })
-map('n', '<leader>h', function()
-    require("oil").toggle_hidden()
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+map("n", "<leader>h", function()
+	require("oil").toggle_hidden()
 end, { desc = "Toggle hidden files in oil.nvim" })
