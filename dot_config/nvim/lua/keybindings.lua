173a12d90c55c28b5ade39acedfa8b2cff81fca6
Davide Aversa <thek3nger@gmail.com>
2025-09-10 21:43:33 +0200

    Update .config/nvim/lua/keybindings.lua Update .config/nvim/lua/plugins/formatter.lua Update .config/nvim/lua/plugins/tree.lua

--- dot_config/nvim/lua/keybindings.lua
+++ dot_config/nvim/lua/keybindings.lua
@@ -13,23 +13,25 @@ end
 
 vim.g.mapleader = ","
 
+-- Exit insert mode
 map("i", "jk", "<Esc>")
-map("n", "<leader>d", ":Neotree toggle=true<CR>", { desc = "Neotree" })
-map("", "<C-t>", "tabnew<CR>")
 
-map("n", "<silent> <A-Up>", ":wincmd k<CR>")
-map("n", "<silent> <A-Down>", ":wincmd j<CR>")
-map("n", "<silent> <A-Left>", ":wincmd h<CR>")
-map("n", "<silent> <A-Right>", ":wincmd l<CR>")
+-- NeoTree
+map("n", "<leader>e", ":Neotree toggle=true<CR>", { desc = "Neotree" })
+map("n", "<leader>r", ":Neotree focus<CR>", { desc = "Focus Neotree" })
+
+-- Tabs
+map("n", "<C-t>", ":tabnew<CR>", { noremap = true, silent = true, desc = "New Tab" })
+map("n", "<C-x>", ":tabclose<CR>", { noremap = true, silent = true, desc = "Cloe Tab" })
 
 -- Find files using Telescope command-line sugar.
-map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true })
-map("n", "<leader>gg", "<cmd>Telescope live_grep<CR>", { noremap = true })
-map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { noremap = true })
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { noremap = true, desc = "Find Files" })
+map("n", "<leader>gg", "<cmd>Telescope live_grep<CR>", { noremap = true, desc = "Live Grep" })
 map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { noremap = true })
-map("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true })
+map("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, desc = "Diagnostics" })
 
 -- Buffers Shortcuts
+map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { noremap = true, desc = "Buffers" })
 map("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
 map("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
 map("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
--- dot_config/nvim/lua/plugins/tree.lua
+++ dot_config/nvim/lua/plugins/tree.lua
@@ -1,59 +1,60 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })()
+	end,
 }
 
 local T = {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "rachartier/tiny-devicons-auto-colors.nvim",
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    opts = {
-        -- use_default_mappings = false,
-        window = {
-            mapping_options = {
-                noremap = true,
-                nowait = true,
-            },
-            mappings = {
-                ["l"] = "open",
-                ["h"] = "close_node",
-                ["!"] = "toggle_hidden",
-                ["d"] = "add_directory",
-                ["X"] = "delete",
-                ["/"] = "noop", -- Disable fuzzy finder thx
-                ["?"] = "noop", -- Disable fuzzy finder thx
-                ["space"] = "none",
-                ["<Esc>"] = "close_window",
-            },
-        },
-        filesystem = {
-            -- Use oil instead
-            hijack_netrw_behavior = "disabled",
-        },
-    },
-    cmd = "Neotree",
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"rachartier/tiny-devicons-auto-colors.nvim",
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	opts = {
+		-- use_default_mappings = false,
+		window = {
+			mapping_options = {
+				noremap = true,
+				nowait = true,
+			},
+			mappings = {
+				["l"] = "open",
+				["h"] = "close_node",
+				["!"] = "toggle_hidden",
+				["d"] = "add_directory",
+				["X"] = "delete",
+				["/"] = "noop", -- Disable fuzzy finder thx
+				["?"] = "noop", -- Disable fuzzy finder thx
+				["space"] = "none",
+				["<Esc>"] = "close_window",
+			},
+		},
+		filesystem = {
+			-- Use oil instead
+			hijack_netrw_behavior = "disabled",
+		},
+	},
+	cmd = "Neotree",
 }
 
 return {
-    M,
-    T,
-    {
-        'stevearc/oil.nvim',
-        ---@module 'oil'
-        ---@type oil.SetupOpts
-        opts = {
-            default_file_explorer = true,
-        },
-        dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-        lazy = false,
-    }
+	M,
+	T,
+	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			default_file_explorer = true,
+		},
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		lazy = false,
+	},
 }
