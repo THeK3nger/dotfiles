bf959c41e3cb6c80072dc6e4b234861dfe7fa695
Davide Aversa <thek3nger@gmail.com>
2025-09-16 11:38:23 +0200

    Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -12,67 +12,14 @@ return {
 	},
 	{
 		"williamboman/mason.nvim",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+		},
 		config = function()
 			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			local lspconfig = require("lspconfig")
-
-			local function root_pattern_excludes(opt)
-				local root = opt.root
-				local exclude = opt.exclude
-
-				local function matches(path, pattern)
-					return 0 < #vim.fn.glob(util.path.join(path, pattern))
-				end
-
-				return function(startpath)
-					return util.search_ancestors(startpath, function(path)
-						return matches(path, root) and not matches(path, exclude)
-					end)
-				end
-			end
-
-			local servers = {
-				denols = {
-					root_dir = root_pattern_excludes({
-						root = "deno.json?",
-						exclude = "package.json",
-					}),
-					single_file_support = false,
-					settings = {},
-				},
-				ts_ls = {
-					root_dir = root_pattern_excludes({
-						root = "package.json",
-						exclude = "deno.json?",
-					}),
-					single_file_support = false,
-					settings = {},
-				},
-
-				lua_ls = {},
-				pyright = {},
-				ols = {},
-				hls = {},
-			}
-
 			require("mason-lspconfig").setup({
-				ensure_installed = vim.tbl_keys(servers),
-				automatic_installation = true,
-				handlers = {
-					-- Default handler - skip ts_ls and denols
-					function(server_name)
-						if server_name == "ts_ls" or server_name == "denols" then
-							return
-						end
-						local server_config = servers[server_name] or {}
-						lspconfig[server_name].setup(server_config)
-					end,
-				},
+				ensure_installed = { "lua_ls", "ts_ls", "pyright", "rust_analyzer", "denols", "ols", "hls" },
+				automatic_enable = true,
 			})
 		end,
 	},
